948a674338c00b9ee4158d18a141a24306d12305
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-16 20:14:27 +0200

    Remove Token submodule

--- lib/shiftplanning/token.ex
+++
@@ -1,7 +0,0 @@
-defmodule Shiftplanning.Token do
-  def token(key \\ Application.get_env(:shiftplanning, :key),
-            username \\ Application.get_env(:shiftplanning, :username),
-            password \\ Application.get_env(:shiftplanning, :password) do
-              
-            end
-end
