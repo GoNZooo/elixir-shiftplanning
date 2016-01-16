6dbf25ebd528cadc4ef36541c9b9a67462f8765f
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-16 20:05:46 +0200

    Rename get() to get_token()
    Previously in Token submodule, but now in Login; more general module

--- lib/shiftplanning/login.ex
+++ lib/shiftplanning/login.ex
@@ -1,17 +1,17 @@
 defmodule Shiftplanning.Login do
   import Shiftplanning.Base, only: [request: 1]
 
-  def get(key \\ Application.get_env(:shiftplanning, :key),
-          username \\ Application.get_env(:shiftplanning, :username),
-          password \\ Application.get_env(:shiftplanning, :password),
-          headers \\ Shiftplanning.Defaults.headers,
-          api_url \\ Shiftplanning.Defaults.api_url) do
+  def token(key \\ Application.get_env(:shiftplanning, :key),
+            username \\ Application.get_env(:shiftplanning, :username),
+            password \\ Application.get_env(:shiftplanning, :password),
+            headers \\ Shiftplanning.Defaults.headers,
+            api_url \\ Shiftplanning.Defaults.api_url) do
     payload = %{key: key,
                 request: %{module: "staff.login",
                            method: "GET",
                            username: username,
                            password: password}}
-    
+
     %HTTPoison.Response{body: body} = request(payload)
     response = Poison.decode!(body)
     response["token"]
