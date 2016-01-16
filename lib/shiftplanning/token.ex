e0f76ff66433264e432b90b01519e583132d95a3
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-16 03:40:17 +0200

    Add Shiftplanning.Token module file

---
+++ lib/shiftplanning/token.ex
@@ -0,0 +1,7 @@
+defmodule Shiftplanning.Token do
+  def token(key \\ Application.get_env(:shiftplanning, :key),
+            username \\ Application.get_env(:shiftplanning, :username),
+            password \\ Application.get_env(:shiftplanning, :password) do
+              
+            end
+end
