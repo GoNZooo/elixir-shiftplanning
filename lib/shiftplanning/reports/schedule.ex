4d98633fcc33506630ed4ff49e5eb921302b360c
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-19 01:52:35 +0200

    Add get_schedule_summary/2 in Schedule module

--- lib/shiftplanning/reports/schedule.ex
+++ lib/shiftplanning/reports/schedule.ex
@@ -1,5 +1,6 @@
 defmodule Shiftplanning.Reports.Schedule do
   import Shiftplanning.Base, only: [request: 1]
+
   def get_shift_confirmed(start_date, end_date,
                           token \\ Shiftplanning.Cache.Token.cached) do
     payload = %{token: token,
@@ -8,6 +9,18 @@ defmodule Shiftplanning.Reports.Schedule do
                            start_date: start_date,
                            end_date: end_date,
                            type: "shift_confirmed"}}
+    request(payload)
+  end
+
+  def get_schedule_summary(start_date, end_date,
+                           token \\ Shiftplanning.Cache.Token.cached) do
+    payload = %{token: token,
+                request: %{module: "reports.schedule",
+                           method: "GET",
+                           start_date: start_date,
+                           end_date: end_date,
+                           type: "schedule_summary"}}
+
     request(payload)
   end
 end
