9d555db6930a6a3d6c2a205af65a6002981f6e02
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-19 13:27:04 +0200

    Add base get() function that type funcs use

--- lib/shiftplanning/reports/schedule.ex
+++ lib/shiftplanning/reports/schedule.ex
@@ -1,26 +1,22 @@
 defmodule Shiftplanning.Reports.Schedule do
   import Shiftplanning.Base, only: [request: 1]
 
-  def get_shift_confirmed(start_date, end_date,
-                          token \\ Shiftplanning.Cache.Token.cached) do
+  defp get(start_date, end_date, type,
+           token \\ Shiftplanning.Cache.Token.cached) do
     payload = %{token: token,
                 request: %{module: "reports.schedule",
                            method: "GET",
                            start_date: start_date,
                            end_date: end_date,
-                           type: "shift_confirmed"}}
+                           type: type}}
     request(payload)
   end
 
-  def get_schedule_summary(start_date, end_date,
-                           token \\ Shiftplanning.Cache.Token.cached) do
-    payload = %{token: token,
-                request: %{module: "reports.schedule",
-                           method: "GET",
-                           start_date: start_date,
-                           end_date: end_date,
-                           type: "schedule_summary"}}
+  def get_shift_confirmed(start_date, end_date) do
+    get(start_date, end_date, "shift_confirmed")
+  end
 
-    request(payload)
+  def get_schedule_summary(start_date, end_date) do
+    get(start_date, end_date, "schedule_summary")
   end
 end
