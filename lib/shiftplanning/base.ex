33cea5843c84551c456743c7a50bf6cd7edb072c
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-29 02:05:33 +0200

    Make data accessing automatic
    Exception is for login request, now detected through matching of the
    payload sent to request/3, where it will access the token key
    automatically instead.

--- lib/shiftplanning/base.ex
+++ lib/shiftplanning/base.ex
@@ -1,10 +1,27 @@
 defmodule Shiftplanning.Base do
-  def request(payload,
-              headers \\ Shiftplanning.Defaults.headers,
-              api_url \\ Shiftplanning.Defaults.api_url) do
+  def request(payload) do
+    request(payload,
+            Shiftplanning.Defaults.headers,
+            Shiftplanning.Defaults.api_url)
+  end
+  def request(payload = %{request: %{module: "staff.login",
+                                     method: "GET"}},
+              headers,
+              api_url) do
     %HTTPoison.Response{body: body} = HTTPoison.post!(api_url,
                                                       "data=#{Poison.encode!(payload)}",
                                                       headers)
-    %{"status" => 1} = Poison.decode!(body)
+    data = %{"status" => 1} = Poison.decode!(body)
+    data["token"]
+  end
+
+  def request(payload,
+              headers,
+              api_url) do
+    %HTTPoison.Response{body: body} = HTTPoison.post!(api_url,
+                                                      "data=#{Poison.encode!(payload)}",
+                                                      headers)
+    data = %{"status" => 1} = Poison.decode!(body)
+    data["data"]
   end
 end
