15bddf1dc2cdc954e822709c52b0385cab6b29d7
Rickard Andersson <gonz@severnatazvezda.com>
2016-01-30 23:42:06 +0200

    Add "manage" mode to Schedule.Vacations.get

--- lib/shiftplanning/schedule/vacations.ex
+++ lib/shiftplanning/schedule/vacations.ex
@@ -1,13 +1,19 @@
 defmodule Shiftplanning.Schedule.Vacations do
   import Shiftplanning.Base, only: [request: 1]
 
-  def get(start_date, end_date,
+  def get(start_date, end_date, mode,
           token \\ Shiftplanning.Cache.Token.cached) do
     payload = %{token: token,
                 request: %{module: "schedule.vacations",
                            method: "GET",
                            start_date: start_date,
-                           end_date: end_date}}
+                           end_date: end_date,
+                           mode: mode}}
     request(payload)
+  end
+
+  def get_manage(start_date, end_date,
+                 token \\ Shiftplanning.Cache.Token.cached) do
+    get(start_date, end_date, "manage", token)
   end
 end
