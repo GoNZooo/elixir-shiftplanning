a7f3933e139d898ab05c55a54dfc1a8e68e43239
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-12 13:17:38 +0200

    Modify vacations get()s; dates are ignored

--- lib/shiftplanning/schedule/vacations.ex
+++ lib/shiftplanning/schedule/vacations.ex
@@ -1,33 +1,31 @@
 defmodule Shiftplanning.Schedule.Vacations do
   import Shiftplanning.Base, only: [request: 1]
 
-  defp _get(start_date, end_date, mode, token) do
+  defp _get(mode, token) do
     payload = %{token: token,
                 request: %{module: "schedule.vacations",
                            method: "GET",
-                           start_date: start_date,
-                           end_date: end_date,
                            mode: mode}}
     request(payload)
   end
 
-  def get(start_date, end_date) do
-    get(:manage, start_date, end_date, Shiftplanning.Cache.Token.cached)
+  def get do
+    get(:manage, Shiftplanning.Cache.Token.cached)
   end
 
-  def get(mode, start_date, end_date) do
-    get(mode, start_date, end_date, Shiftplanning.Cache.Token.cached)
+  def get(mode) do
+    get(mode, Shiftplanning.Cache.Token.cached)
   end
 
-  def get(:manage, start_date, end_date, token) do
-    _get(start_date, end_date, "manage", token)
+  def get(:manage, token) do
+    _get("manage", token)
   end
 
-  def get(:requested, start_date, end_date, token) do
-    _get(start_date, end_date, "requested", token)
+  def get(:requested, token) do
+    _get("requested", token)
   end
 
-  def get(:upcoming, start_date, end_date, token) do
-    _get(start_date, end_date, "upcoming", token)
+  def get(:upcoming, token) do
+    _get("upcoming", token)
   end
 end
