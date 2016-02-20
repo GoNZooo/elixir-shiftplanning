e94b3be3133ab60ef454a9259543ab06e30c25cc
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-20 12:51:58 +0200

    Flesh out Schedule.Conflicts module more

--- lib/shiftplanning/schedule/conflicts.ex
+++ lib/shiftplanning/schedule/conflicts.ex
@@ -14,11 +14,28 @@ defmodule Shiftplanning.Schedule.Conflicts do
     request(payload)
   end
 
+  defp _get(start_date, end_date, token) do
+    payload = %{token: token,
+                request: %{module: "schedule.conflicts",
+                           method: "GET",
+                           start_date: start_date,
+                           end_date: end_date}}
+    request(payload)
+  end
+
   def get do
     get(Token.cached)
   end
 
   def get(token) do
     _get(token)
+  end
+
+  def get(start_date, end_date) do
+    get(start_date, end_date, Token.cached)
+  end
+
+  def get(start_date, end_date, token) do
+    _get(start_date, end_date, token)
   end
 end
