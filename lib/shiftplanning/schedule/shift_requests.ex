93b5d78cc49a3ecfa53bbcb401af7bf6ca92a562
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-21 20:45:52 +0200

    Add Schedule.ShiftRequests module

---
+++ lib/shiftplanning/schedule/shift_requests.ex
@@ -0,0 +1,20 @@
+def Shiftplanning.Schedule.ShiftRequests do
+  import Shiftplanning.Base, only: [request: 1]
+  alias Shiftplanning.Cache.Token
+
+  defp _get(shift, token) do
+    payload = %{token: token,
+                request: %{module: "schedule.shiftrequests",
+                           method: "GET",
+                           shift: shift}}
+    request(payload)
+  end
+
+  def get(shift) do
+    get(shift, Token.cached)
+  end
+
+  def get(shift, token) do
+    _get(shift, token)
+  end
+end
