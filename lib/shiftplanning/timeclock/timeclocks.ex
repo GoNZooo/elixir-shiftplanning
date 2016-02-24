26f87b3642b8f430dbf00a8226a4bf182fe40b65
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-24 22:48:31 +0200

    Add search for specific schedule in Timeclock.Timeclocks

--- lib/shiftplanning/timeclock/timeclocks.ex
+++ lib/shiftplanning/timeclock/timeclocks.ex
@@ -9,11 +9,27 @@ defmodule Shiftplanning.Timeclock.Timeclocks do
     request(payload)
   end
 
+  defp _get(%{schedule: schedule}, token) do
+    payload = %{token: token,
+                request: %{module: "timeclock.timeclocks",
+                           method: "GET",
+                           schedule: schedule}}
+    request(payload)
+  end
+
   def get do
     get(Token.cached)
   end
 
   def get(token) do
     _get(token)
+  end
+
+  def get(%{schedule: schedule}) do
+    get(%{schedule: schedule}, token)
+  end
+
+  def get(%{schedule: schedule}, token) do
+    _get(%{schedule: schedule}, token)
   end
 end
