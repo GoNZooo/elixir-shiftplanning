9d3ddaf01dbf45df3d7e2dab18395c9a144ca9ce
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-25 19:30:36 +0200

    Add specs to Defaults

--- lib/shiftplanning/defaults.ex
+++ lib/shiftplanning/defaults.ex
@@ -1,9 +1,11 @@
 defmodule Shiftplanning.Defaults do
+  @type headers :: [{String.t, String.t}]
   def headers do
     [{"Accept", "application/json"},
     {"Content-Type", "application/x-www-form-urlencoded"}]
   end
 
+  @type api_url :: String.t
   def api_url do
     "https://www.shiftplanning.com/api/"
   end
