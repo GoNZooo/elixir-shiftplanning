1dfbe6fe86a7fd69488afec72a9e9662a4405d9f
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-28 23:50:59 +0200

    Add @specs to Cache.Token module

--- lib/shiftplanning/cache/token.ex
+++ lib/shiftplanning/cache/token.ex
@@ -1,8 +1,10 @@
 defmodule Shiftplanning.Cache.Token do
+  @spec start_link :: {:ok, pid}
   def start_link do
     Agent.start_link(fn -> Shiftplanning.Staff.Login.get end, name: __MODULE__)
   end
 
+  @spec cached :: String.t
   def cached do
     Agent.get(__MODULE__, fn token -> token end)
   end
