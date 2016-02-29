481af1b5770ca048aef3fc78d4b46f5d6f58475c
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-29 23:05:26 +0200

    Add specs to Schedule.Schedules module

--- lib/shiftplanning/schedule/schedules.ex
+++ lib/shiftplanning/schedule/schedules.ex
@@ -2,6 +2,7 @@ defmodule Shiftplanning.Schedule.Schedules do
   alias Shiftplanning.Cache.Token
   import Shiftplanning.Base, only: [request: 1]
 
+  @spec _get(1 | 0, String.t) :: map()
   defp _get(perms, token) do
     payload = %{token: token,
                 request: %{module: "schedule.schedules",
@@ -10,12 +11,12 @@ defmodule Shiftplanning.Schedule.Schedules do
     request(payload)
   end
 
-  @spec get(1 | 0) :: 
+  @spec get(1 | 0) :: map()
   def get(perms) do
     get(perms, Token.cached)
   end
 
-  @spec get(1 | 0, String.t)
+  @spec get(1 | 0, String.t) :: map()
   def get(perms, token) do
     _get(perms, token)
   end
