5844c3dfbd840e4c95ca05c3b4bc7661c4c9d121
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-29 23:05:44 +0200

    Add specs to Schedule.Vacations module

--- lib/shiftplanning/schedule/vacations.ex
+++ lib/shiftplanning/schedule/vacations.ex
@@ -9,22 +9,27 @@ defmodule Shiftplanning.Schedule.Vacations do
     request(payload)
   end
 
+  @spec get :: map
   def get do
     get(:manage, Shiftplanning.Cache.Token.cached)
   end
 
+  @spec get(String.t) :: map
   def get(mode) do
     get(mode, Shiftplanning.Cache.Token.cached)
   end
 
+  @spec get(:manage, String.t) :: map
   def get(:manage, token) do
     _get("manage", token)
   end
 
+  @spec get(:requested, String.t) :: map
   def get(:requested, token) do
     _get("requested", token)
   end
 
+  @spec get(:upcoming, String.t) :: map
   def get(:upcoming, token) do
     _get("upcoming", token)
   end
