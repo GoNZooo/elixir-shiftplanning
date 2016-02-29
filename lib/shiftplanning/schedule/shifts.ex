e0ec01c4a359788474b0fb69e927cd74cb37f149
Rickard Andersson <gonz@severnatazvezda.com>
2016-02-29 23:15:08 +0200

    Make Schedule.Shifts module follow pattern
    Also add spec information as well as mode for :confirm

--- lib/shiftplanning/schedule/shifts.ex
+++ lib/shiftplanning/schedule/shifts.ex
@@ -1,15 +1,42 @@
 defmodule Shiftplanning.Schedule.Shifts do
   import Shiftplanning.Base, only: [request: 1]
+  alias Shiftplanning.Cache.Token
 
-  def get_overview(start_date, end_date,
-                   token \\ Shiftplanning.Cache.Token.cached) do
+  @spec _get(String.t, String.t, String.t, String.t) :: map
+  def _get(mode, start_date, end_date,
+           token \\ Shiftplanning.Cache.Token.cached) do
     payload = %{token: token,
                 request: %{module: "schedule.shifts",
                            method: "GET",
                            start_date: start_date,
                            end_date: end_date,
-                           mode: "overview"}}
+                           mode: mode}}
 
     request(payload)
+  end
+
+  @spec get(String.t, String.t) :: map
+  def get(start_date, end_date) do
+    get(:overview, start_date, end_date, Token.cached)
+  end
+
+  @spec get(:overview, String.t, String.t) :: map
+  def get(:overview, start_date, end_date) do
+    get(:overview, start_date, end_date, Token.cached)
+  end
+
+  @spec get(:confirm, String.t, String.t) :: map
+  def get(:confirm, start_date, end_date) do
+    get(:confirm, start_date, end_date, Token.cached)
+  end
+
+  @spec get(:overview, String.t, String.t, String.t) :: map
+  def get(:overview, start_date, end_date, token) do
+    _get("overview", start_date, end_date, token)
+  end
+
+  @spec get(:confirm, String.t, String.t, String.t) :: map
+  def get(:confirm, start_date, end_date, token) do
+    _get("confirm", start_date, end_date, token)
   end
 end
