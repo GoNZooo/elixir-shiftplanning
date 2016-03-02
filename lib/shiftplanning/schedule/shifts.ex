d64c9b9ebbf1562573a10e9a6b3256c6b2fb80da
Rickard Andersson <gonz@severnatazvezda.com>
2016-03-02 23:05:07 +0200

    Fix so that fetching of employees' shift works

--- lib/shiftplanning/schedule/shifts.ex
+++ lib/shiftplanning/schedule/shifts.ex
@@ -3,13 +3,26 @@ defmodule Shiftplanning.Schedule.Shifts do
   alias Shiftplanning.Cache.Token
 
   @spec _get(String.t, String.t, String.t, String.t) :: map
-  def _get(mode, start_date, end_date,
-           token \\ Shiftplanning.Cache.Token.cached) do
+  def _get(mode, start_date, end_date, token) do
     payload = %{token: token,
                 request: %{module: "schedule.shifts",
                            method: "GET",
                            start_date: start_date,
                            end_date: end_date,
+                           mode: mode}}
+
+    request(payload)
+  end
+
+  @spec _get(String.t, String.t, String.t, String.t,
+             %{employees: [String.t]}) :: map
+  def _get(mode, start_date, end_date, token, %{employees: employees}) do
+    payload = %{token: token,
+                request: %{module: "schedule.shifts",
+                           method: "GET",
+                           start_date: start_date,
+                           end_date: end_date,
+                           employees: employees,
                            mode: mode}}
 
     request(payload)
@@ -35,6 +48,21 @@ defmodule Shiftplanning.Schedule.Shifts do
     get(:location, start_date, end_date, Token.cached)
   end
 
+  @spec get(:schedule, String.t, String.t) :: map
+  def get(:schedule, start_date, end_date) do
+    get(:schedule, start_date, end_date, Token.cached)
+  end
+
+  @spec get(:incomplete, String.t, String.t) :: map
+  def get(:incomplete, start_date, end_date) do
+    get(:incomplete, start_date, end_date, Token.cached)
+  end
+
+  @spec get(:employees, String.t, String.t, %{employees: [String.t]}) :: map
+  def get(:employees, start_date, end_date, %{employees: employees}) do
+    get(:employees, start_date, end_date, Token.cached, %{employees: employees})
+  end
+
   @spec get(:overview, String.t, String.t, String.t) :: map
   def get(:overview, start_date, end_date, token) do
     _get("overview", start_date, end_date, token)
@@ -48,5 +76,22 @@ defmodule Shiftplanning.Schedule.Shifts do
   @spec get(:location, String.t, String.t, String.t) :: map
   def get(:location, start_date, end_date, token) do
     _get("location", start_date, end_date, token)
+  end
+
+  @spec get(:schedule, String.t, String.t, String.t) :: map
+  def get(:schedule, start_date, end_date, token) do
+    _get("schedule", start_date, end_date, token)
+  end
+
+  @spec get(:incomplete, String.t, String.t, String.t) :: map
+  def get(:incomplete, start_date, end_date, token) do
+    _get("incomplete", start_date, end_date, token)
+  end
+
+  @spec get(:employees, String.t, String.t, String.t,
+            %{employees: [String.t]}) :: map
+  def get(:employees, start_date, end_date, token, %{employees: employees}) do
+    employee_list = Enum.join(employees, ",")
+    _get("employees", start_date, end_date, token, %{employees: employee_list})
   end
 end
